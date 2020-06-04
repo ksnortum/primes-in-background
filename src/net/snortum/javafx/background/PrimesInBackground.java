6e877a3310a5209d4c605a7d13bd80da0339e5dc
Knute Snortum <knute@snortum.net>
2020-06-04 15:24:55 -0700

    Split file into two classes

--- src/net/snortum/javafx/background/PrimesInBackground.java
+++ src/net/snortum/javafx/background/PrimesInBackground.java
@@ -1,7 +1,6 @@
 package net.snortum.javafx.background;
 
 import javafx.application.Application;
-import javafx.concurrent.Task;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -80,71 +79,4 @@ public class PrimesInBackground extends Application {
 		primaryStage.show();
 	}
 
-}
-
-class PrimeNumbersTask extends Task<Void> { 
-	private int numbersToFind;
-	private boolean stopTask = false;
-
-	PrimeNumbersTask(int numbersToFind) {
-		this.numbersToFind = numbersToFind;
-	}
-
-	@Override
-	protected Void call() throws Exception {
-		long number = 0;
-		
-		for (int i = 0; i < numbersToFind; i++) {
-			if (stopTask) {
-				updateProgress(0, numbersToFind);
-				break;
-			}
-			
-			number = nextPrimeNumber(number);
-			// You get the value of number from the new value of the listener 
-			// of a message property in the calling class
-			updateMessage(String.valueOf(number));
-			// You get the progress from binding the task's and progressbar's progress properties
-			updateProgress(i + 1, numbersToFind);
-			// slow down creation of primes so the display can keep up
-			// (probably not needed if you are binding to task's message property)
-			Thread.sleep(10); 
-		}
-		
-		return null; 
-	}
-	
-	public void setStopTask(boolean stopTask) {
-		this.stopTask = stopTask;
-	}
-
-	private long nextPrimeNumber(long thisPrime) {
-		if (thisPrime < 2) {
-			return 2;
-		} else if (thisPrime == 2) {
-			return 3;
-		} else if (thisPrime == 3) {
-			return 5;
-		}
-		
-		long candidate = thisPrime;
-		candidate += (thisPrime % 2 == 0) ? 1 : 2;
-		
-		while (true) {
-			boolean noDivisor = true;
-			
-			for (int i = 3; i < Math.sqrt(candidate); i += 2) {
-				if (candidate % i == 0) {
-					noDivisor = false;
-					break;
-				}
-			}
-			
-			if (noDivisor) {
-				return candidate;
-			}
-			
-			candidate += 2;
-		}
-	}
 }
